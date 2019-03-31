193e3d5b766dd4ff5eed5865a7ee8983f7968ecf
Gregor Dschung <gregor.dschung@andrena.de>
2019-03-31 10:15:13 +0200

    Some more targets

---
+++ MaultaschenData/MaultaschenData.h
@@ -0,0 +1,19 @@
+//
+//  MaultaschenData.h
+//  MaultaschenData
+//
+//  Created by Gregor Dschung on 31.03.19.
+//  Copyright © 2019 Gregor Dschung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaultaschenData.
+FOUNDATION_EXPORT double MaultaschenDataVersionNumber;
+
+//! Project version string for MaultaschenData.
+FOUNDATION_EXPORT const unsigned char MaultaschenDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaultaschenData/PublicHeader.h>
+
+
---
+++ MaultaschenDomain/MaultaschenDomain.h
@@ -0,0 +1,19 @@
+//
+//  MaultaschenDomain.h
+//  MaultaschenDomain
+//
+//  Created by Gregor Dschung on 31.03.19.
+//  Copyright © 2019 Gregor Dschung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaultaschenDomain.
+FOUNDATION_EXPORT double MaultaschenDomainVersionNumber;
+
+//! Project version string for MaultaschenDomain.
+FOUNDATION_EXPORT const unsigned char MaultaschenDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaultaschenDomain/PublicHeader.h>
+
+
