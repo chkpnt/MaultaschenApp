8ca31925af3588476b1c5bf0b259992e5082fe8a
Gregor Dschung <gregor.dschung@andrena.de>
2019-03-31 10:01:08 +0200

    Auf geht’s, ab geht’s, 3 Tage wach!

---
+++ MaultaschenApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MaultaschenApp
+//
+//  Created by Gregor Dschung on 31.03.19.
+//  Copyright © 2019 Gregor Dschung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
